3e03075536f87098c8fd4e597d19fb7be8cc3712
Dénes Urbán <urband@edu.bme.hu>
2022-09-20 15:27:38 +0200

    almost done except the choosing thing

--- ViewLabor/app/src/main/java/hu/bme/aut/android/viewlabor/MainActivity.kt
+++ ViewLabor/app/src/main/java/hu/bme/aut/android/viewlabor/MainActivity.kt
@@ -2,10 +2,25 @@ package hu.bme.aut.android.viewlabor
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import codes.side.andcolorpicker.group.PickerGroup
+import codes.side.andcolorpicker.group.registerPickers
+import codes.side.andcolorpicker.model.IntegerHSLColor
+import hu.bme.aut.android.viewlabor.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val pickerGroup = PickerGroup<IntegerHSLColor>()
+        pickerGroup.registerPickers(
+            binding.hueSeekBar,
+            binding.saturationSeekBar,
+            binding.lightnessSeekBar,
+            binding.alphaSeekBar
+        )
     }
 }
