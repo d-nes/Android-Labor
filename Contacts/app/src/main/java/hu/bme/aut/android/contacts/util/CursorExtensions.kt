d0d2c6e1f068e7761ff0dc5bbeab7db2ce28cbc8
Dénes Urbán <100215101+d-nes@users.noreply.github.com>
2022-10-15 09:50:45 +0200

    lets start again eyo

--- Contacts/app/src/main/java/hu/bme/aut/android/contacts/util/CursorExtensions.kt
+++
@@ -1,7 +0,0 @@
-package hu.bme.aut.android.contacts.util
-
-import android.annotation.SuppressLint
-import android.database.Cursor
-
-@SuppressLint("Range")
-fun Cursor.getStringByColumnName(colName: String) = this.getString(this.getColumnIndex(colName))
